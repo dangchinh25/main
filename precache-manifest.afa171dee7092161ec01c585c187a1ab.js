f3cd4bdd2035397de46df3fb2643392acfb0c397
DangChinh <35798337+DangChinh@users.noreply.github.com>
2019-10-31 01:40:00 -0500

    Updates

---
+++ precache-manifest.afa171dee7092161ec01c585c187a1ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5cec721ea796d82261f3d73fa50dc33",
+    "url": "/main/index.html"
+  },
+  {
+    "revision": "ff32b1c9c8dfa5d273fa",
+    "url": "/main/static/css/main.0168050c.chunk.css"
+  },
+  {
+    "revision": "ddcbdf7ab8a42e3390da",
+    "url": "/main/static/js/2.13b8bc67.chunk.js"
+  },
+  {
+    "revision": "ff32b1c9c8dfa5d273fa",
+    "url": "/main/static/js/main.afc540b1.chunk.js"
+  },
+  {
+    "revision": "82d0e4edb4251c9fc8eb",
+    "url": "/main/static/js/runtime-main.5c33063b.js"
+  }
+]);
